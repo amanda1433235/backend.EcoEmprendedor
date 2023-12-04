d7b99a9b5d33d80a7999e97335cb642a7b8c3c92
Izadora <Izadora@DESKTOP-MJSLBVS>
2023-12-03 23:29:20 -0300

    Adicionar arquivos de projeto.

---
+++ backend.EcoEmprendedor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace backend.EcoEmprendedor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
